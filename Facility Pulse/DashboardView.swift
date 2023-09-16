b03fd97db19e55d4b8a638cb3b6d6584003ea0a6
ntdkhang <ntdkhang143@gmail.com>
2023-09-16 16:30:01 -0500

    update gitignore, move to ios 16.1

---
+++ Facility Pulse/DashboardView.swift
@@ -0,0 +1,24 @@
+//
+//  DashboardView.swift
+//  Facility Pulse
+//
+//  Created by Khang Nguyen on 9/16/23.
+//
+
+import SwiftUI
+
+struct DashboardView: View {
+    var body: some View {
+        VStack {
+            List {
+                
+            }
+        }
+    }
+}
+
+struct DashboardView_Previews: PreviewProvider {
+    static var previews: some View {
+        DashboardView()
+    }
+}
