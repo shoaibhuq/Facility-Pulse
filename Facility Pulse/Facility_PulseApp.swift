21a0d130bed88c7d3acc6f720f48d7c0c789da19
Shoaib Huq <slimsho123@gmail.com>
2023-09-16 14:17:01 -0500

    Initial Commit

---
+++ Facility Pulse/Facility_PulseApp.swift
@@ -0,0 +1,17 @@
+//
+//  Facility_PulseApp.swift
+//  Facility Pulse
+//
+//  Created by Shoaib Huq on 9/16/23.
+//
+
+import SwiftUI
+
+@main
+struct Facility_PulseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
